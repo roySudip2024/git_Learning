9e90690eef9a5bc9ae75b1ae3b6e89bc21f17ac3
Sudip Roy <sudipqueensu@gmail.com>
2026-03-22 18:15:54 -0400

    Secind commit with updated file.

--- new_file.py
+++ new_file.py
@@ -1,2 +1,4 @@
 # This is my first Python file. I am learning how to write code in Python. I will be able to create more complex programs as I learn more about the language. I am excited to see what I can create with Python!
 print("Hello, World!")
+
+# This is a simple program that prints "Hello, World!" to the console. It is a common first program for beginners to learn how to write code in a new programming language. I am happy to have written my first Python program! 
