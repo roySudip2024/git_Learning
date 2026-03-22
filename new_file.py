5650b99765944037933e365af4879ebaf45518e4
Sudip Roy <sudipqueensu@gmail.com>
2026-03-22 18:35:44 -0400

    Commiting file after merger

--- new_file.py
+++ new_file.py
@@ -2,3 +2,6 @@
 print("Hello, World!")
 
 # This is a simple program that prints "Hello, World!" to the console. It is a common first program for beginners to learn how to write code in a new programming language. I am happy to have written my first Python program! 
+# this is the 3rd line of code in this file. I am learning how to write comments in Python. Comments are important for explaining what your code does and making it easier for others to understand. I will make sure to use comments in my future programs to help others understand my code better.   
+
+print("I am learning Python!")
